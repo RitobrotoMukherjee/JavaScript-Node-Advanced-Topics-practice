a770eb5de639bac9606cbc53b55fd1bc4b0dae19
Ritobroto Mukherjee <ritobrotomukherjee1991@gmail.com>
2026-01-12 18:40:12 +0530

    Read Write and Delete + Synchronous and Async Functionality of FS module

--- File-System-Play/writeFile.js
+++ File-System-Play/writeFile.js
@@ -1,1 +1,43 @@
 console.log("Create and Write to newFile.txt");
+
+const fs = require("node:fs");
+
+const written = fs.writeFileSync("newFileSync.txt", "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do", "utf-8");
+
+console.log("File written successfully:", written === undefined ? "No return value" : written);
+
+let content = fs.readFileSync("newFileSync.txt", "utf-8");
+console.log("Content of newFileSync.txt:", content, "\n");
+
+/*
+    Read and write from a file
+    using async operations
+*/
+
+try {
+    fs.rmdirSync("AsyncDir", { recursive: true });
+} catch (err) {
+    console.error("Error removing directory:");
+}
+
+try {
+    fs.mkdirSync("AsyncDir");
+} catch (err) {
+    console.error("Error creating directory:");
+}
+
+fs.writeFile("AsyncDir/newFileAsync.txt", content, "utf-8", (err) => {
+    if (err) {
+        console.error("Error writing file:", err);
+        return;
+    }
+    console.log("\nAsync file written successfully");
+});
+
+content = fs.readFile("AsyncDir/newFileAsync.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.error("Error reading file:", err);
+        return;
+    }
+    console.log("\nContent of AsyncDir/newFileAsync.txt:", data);
+});
